7c535732243993cbdff9b50b97e4863fce21d816
Khoa Nguyen <khoa.nguyen@TOR-0805.local>
2013-07-07 21:11:55 -0400

    Added new module that talks to the chair using Electric Imp via HTTP GET requests.

---
+++ server/server.js
@@ -0,0 +1,66 @@
+//DEPENDANCIES
+var express = require('express')
+  , path = require('path')
+  , engine = require('ejs-locals')
+  , io = require('socket.io')
+  , serialport = require('serialport')
+  , exec = require('child_process').exec
+  , app = express()
+  , events = require('events')  
+  , arduino
+  , aduinoEventEmitter = new events.EventEmitter()
+  ,	aduinoState = {
+		isConnected : false
+		,lastData : undefined
+	}
+
+
+//CONFIG
+app.configure(function(){
+	app.use(express.compress());
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
+	//app.use(express.favicon(path.join(__dirname + '/public/favicon.ico'))); 
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));  
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+
+//start socket IO
+io = io.listen(app.listen(app.get('port')), function(){
+	console.log("Express server listening on port " + app.get('port'));
+});
+
+
+//ROUTS
+app.get('/', function(req, res) {
+  res.render('index', { locals : { title: 'Smart Ass' }})
+});
+
+app.get('/chair-status', function(req, res) {
+	console.log(req.query.value);
+})
+
+
+//io.sockets.emit('chair', {type : "disconnect", isConnected : aduinoState.isConnected});
+//socket.emit('chair', {type : "init", value:  aduinoState.lastData, isConnected : aduinoState.isConnected});
+//io.sockets.emit('chair', {type : "update", value:  data, isConnected : aduinoState.isConnected});
+
+
+//when new socket is connected
+io.sockets.on('connection', function (socket){
+	console.log("New socket connection made");
+	socket.emit('arduino', {type : "init", value:  aduinoState.lastData, isConnected : aduinoState.isConnected});
+});
